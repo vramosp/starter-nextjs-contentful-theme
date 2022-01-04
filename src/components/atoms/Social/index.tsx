3cbc934549fd20f46af4cca8b41a4c42813add80
Johannes Schickling <schickling.j@gmail.com>
2022-01-04 12:41:37 +0100

    Revert "chore: Change `tabWidth` from 4 to 2"

--- src/components/atoms/Social/index.tsx
+++ src/components/atoms/Social/index.tsx
@@ -11,46 +11,46 @@ import Vimeo from '../../svgs/vimeo';
 import YouTube from '../../svgs/youtube';
 
 const iconMap = {
-  facebook: Facebook,
-  github: GitHub,
-  instagram: Instagram,
-  linkedin: LinkedIn,
-  reddit: Reddit,
-  twitter: Twitter,
-  vimeo: Vimeo,
-  youtube: YouTube
+    facebook: Facebook,
+    github: GitHub,
+    instagram: Instagram,
+    linkedin: LinkedIn,
+    reddit: Reddit,
+    twitter: Twitter,
+    vimeo: Vimeo,
+    youtube: YouTube
 };
 
 export default function Social(props) {
-  const { label, altText, url } = props;
-  const icon = props.icon || 'facebook';
-  const IconComponent = iconMap[icon];
-  const annotationPrefix = props['data-sb-field-path'] || '';
-  const annotations = [
-    `${annotationPrefix}`,
-    `${annotationPrefix}.url#@href`,
-    `${annotationPrefix}.altText#@aria-label`,
-    `${annotationPrefix}.elementId#@id`,
-    `${annotationPrefix}.label#span[1]`,
-    `${annotationPrefix}.icon#svg[1]`
-  ];
-  const style = props.style || 'link';
-  const cssClasses = props.className || null;
-  const cssId = props.elementId || null;
+    const { label, altText, url } = props;
+    const icon = props.icon || 'facebook';
+    const IconComponent = iconMap[icon];
+    const annotationPrefix = props['data-sb-field-path'] || '';
+    const annotations = [
+        `${annotationPrefix}`,
+        `${annotationPrefix}.url#@href`,
+        `${annotationPrefix}.altText#@aria-label`,
+        `${annotationPrefix}.elementId#@id`,
+        `${annotationPrefix}.label#span[1]`,
+        `${annotationPrefix}.icon#svg[1]`
+    ];
+    const style = props.style || 'link';
+    const cssClasses = props.className || null;
+    const cssId = props.elementId || null;
 
-  return (
-    <Link
-      href={url}
-      aria-label={altText}
-      id={cssId}
-      className={classNames('sb-component', 'sb-component-block', 'sb-component-social', cssClasses, {
-        'sb-component-social-primary': style === 'primary',
-        'sb-component-social-secondary': style === 'secondary'
-      })}
-      data-sb-field-path={annotations.join(' ').trim()}
-    >
-      {label && <span className="sr-only">{label}</span>}
-      {IconComponent && <IconComponent className="fill-current h-5 w-5" />}
-    </Link>
-  );
+    return (
+        <Link
+            href={url}
+            aria-label={altText}
+            id={cssId}
+            className={classNames('sb-component', 'sb-component-block', 'sb-component-social', cssClasses, {
+                'sb-component-social-primary': style === 'primary',
+                'sb-component-social-secondary': style === 'secondary'
+            })}
+            data-sb-field-path={annotations.join(' ').trim()}
+        >
+            {label && <span className="sr-only">{label}</span>}
+            {IconComponent && <IconComponent className="fill-current h-5 w-5" />}
+        </Link>
+    );
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '../css/main.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
