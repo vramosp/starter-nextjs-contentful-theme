ebed88d40376ec6f202092f78e98f0143bac956f
Johannes Schickling <schickling.j@gmail.com>
2022-01-04 11:09:59 +0000

    chore: Change `tabWidth` from 4 to 2

--- src/utils/get-data-attrs.ts
+++ src/utils/get-data-attrs.ts
@@ -1,8 +1,8 @@
 export function getDataAttrs(props: any = {}): any {
-    return Object.entries(props).reduce((dataAttrs, [key, value]) => {
-        if (key.startsWith('data-')) {
-            dataAttrs[key] = value;
-        }
-        return dataAttrs;
-    }, {});
+  return Object.entries(props).reduce((dataAttrs, [key, value]) => {
+    if (key.startsWith('data-')) {
+      dataAttrs[key] = value;
+    }
+    return dataAttrs;
+  }, {});
 }
--- src/utils/get-page-url-path.ts
+++ src/utils/get-page-url-path.ts
@@ -1,12 +1,12 @@
 export default function getPageUrlPath(page) {
-    const filePath = page.__metadata?.relSourcePath || '';
-    const parts = filePath.split('/').filter(Boolean);
-    const extensionsRe = /\.(?:mdx?|json|ya?ml|toml)$/;
-    if (parts[parts.length - 1]) {
-        parts[parts.length - 1] = parts[parts.length - 1].replace(extensionsRe, '');
-        if (parts[parts.length - 1] === 'index') {
-            parts.pop();
-        }
+  const filePath = page.__metadata?.relSourcePath || '';
+  const parts = filePath.split('/').filter(Boolean);
+  const extensionsRe = /\.(?:mdx?|json|ya?ml|toml)$/;
+  if (parts[parts.length - 1]) {
+    parts[parts.length - 1] = parts[parts.length - 1].replace(extensionsRe, '');
+    if (parts[parts.length - 1] === 'index') {
+      parts.pop();
     }
-    return '/' + parts.join('/').toLowerCase();
+  }
+  return '/' + parts.join('/').toLowerCase();
 }
